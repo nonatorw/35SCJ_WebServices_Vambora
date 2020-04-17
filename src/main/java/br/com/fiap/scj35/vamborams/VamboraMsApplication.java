552440087dbaa07d04148a28a089a63dc1253df3
wellington.nonato@midwayfinanceira.com.br <wellington.nonato@midway.com.br>
2020-04-16 22:22:30 -0300

    Boot do projeto

---
+++ src/main/java/br/com/fiap/scj35/vamborams/VamboraMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scj35.vamborams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamboraMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VamboraMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/scj35/vamborams/VamboraMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scj35.vamborams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamboraMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
